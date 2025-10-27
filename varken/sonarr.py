2d9ca95f7ceb4ef29d0a2994b22c3372c5421c56
Rexx3270 <modesto.tony@gmail.com>
2025-10-27 11:38:46 -0500

    Update sonarr.py
    Updated for Sonarr API V3+

--- varken/sonarr.py
+++ varken/sonarr.py
@@ -20,15 +20,13 @@ class SonarrAPI(object):
         return f"<sonarr-{self.server.id}>"
 
     def get_calendar(self, query="Missing"):
-        endpoint = '/api/calendar/'
+        endpoint = '/api/v3/calendar/'
         today = str(date.today())
         last_days = str(date.today() - timedelta(days=self.server.missing_days))
         future = str(date.today() + timedelta(days=self.server.future_days))
         now = datetime.now(timezone.utc).astimezone().isoformat()
-        if query == "Missing":
-            params = {'start': last_days, 'end': today}
-        else:
-            params = {'start': today, 'end': future}
+        params = {'start': last_days if query == "Missing" else today,
+                  'end': today if query == "Missing" else future}
         influx_payload = []
         air_days = []
         missing = []
@@ -44,21 +42,23 @@ class SonarrAPI(object):
             try:
                 tv_shows.append(SonarrTVShow(**show))
             except TypeError as e:
-                self.logger.error('TypeError has occurred : %s while creating SonarrTVShow structure for show. Data '
-                                  'attempted is: %s', e, show)
+                self.logger.error('TypeError has occurred : while creating SonarrTVShow: %s - data: %s', e, show)
 
         for show in tv_shows:
             sxe = f'S{show.seasonNumber:0>2}E{show.episodeNumber:0>2}'
-            if show.hasFile:
-                downloaded = 1
-            else:
-                downloaded = 0
+            downloaded = 1 if getattr(show, 'hasFile', False) else 0
+            
+            series_title = show.series.get('title', 'Unknown Series')
+            episode_title = getattr(show, 'title', 'Unknown Episode')
+            air_date_utc = getattr(show, 'airDateUtc', None)
+            show_id = getattr(show, 'id', 0)
+            
             if query == "Missing":
-                if show.monitored and not downloaded:
-                    missing.append((show.series['title'], downloaded, sxe, show.title, show.airDateUtc, show.id))
+                if getattr(show, 'monitored', False) and not downloaded:
+                    missing.append((series_title, downloaded, sxe, episode_title, air_date_utc, show_id))
             else:
-                air_days.append((show.series['title'], downloaded, sxe, show.title, show.airDateUtc, show.id))
-
+                air_days.append((series_title, downloaded, sxe, episode_title, air_date_utc, show_id))
+                
         for series_title, dl_status, sxe, episode_title, air_date_utc, sonarr_id in (air_days or missing):
             hash_id = hashit(f'{self.server.id}{series_title}{sxe}')
             influx_payload.append(
@@ -82,10 +82,14 @@ class SonarrAPI(object):
             )
 
         self.dbmanager.write_points(influx_payload)
+        if influx_payload:
+            self.dbmanager.write_points(influx_payload)
+        else:
+            self.logger.debug("No Sonarr %s data to send to InfluxDB for server %s.", query, self.server.id)
 
     def get_queue(self):
         influx_payload = []
-        endpoint = '/api/queue'
+        endpoint = '/api/v3/queue'
         now = datetime.now(timezone.utc).astimezone().isoformat()
         queue = []
 
@@ -100,26 +104,36 @@ class SonarrAPI(object):
             try:
                 download_queue.append(Queue(**show))
             except TypeError as e:
-                self.logger.error('TypeError has occurred : %s while creating Queue structure. Data attempted is: '
-                                  '%s', e, show)
+                self.logger.error('TypeError while creating Queue structure: %s - data: %s', e, show)
+                
         if not download_queue:
             return
 
         for show in download_queue:
             try:
-                sxe = f"S{show.episode['seasonNumber']:0>2}E{show.episode['episodeNumber']:0>2}"
-            except TypeError as e:
-                self.logger.error('TypeError has occurred : %s while processing the sonarr queue. \
-                                  Remove invalid queue entry. Data attempted is: %s', e, show)
+                episode = getattr(show, 'episode', {})
+                sxe = f"S{episode.get('seasonNumber', 0):0>2}E{episode.get('episodeNumber', 0):0>2}"
+                protocol = getattr(show, 'protocol', 'UNKNOWN').upper()
+                protocol_id = 1 if protocol == 'USENET' else 0
+                
+                quality_data = getattr(show, 'quality', {})
+                if isinstance(quality_data, dict):
+                    quality_name = (
+                        quality_data.get('quality', {}).get('name')
+                        or quality_data.get('name')
+                        or 'Unknown'
+                    )
+                else:
+                    quality_name = str(quality_data)
+                
+                series_title = show.series.get('title', 'Unknown Series')
+                episode_title = episode.get('title', 'Unknown Episode')
+                
+                queue.append((series_title, episode_title, protocol, protocol_id, sxe, show.id, quality_name))
+                
+                except Exception as e:
+                    self.logger.error('Error processing Sonarr queue item: %s', e)
                 continue
-
-            if show.protocol.upper() == 'USENET':
-                protocol_id = 1
-            else:
-                protocol_id = 0
-
-            queue.append((show.series['title'], show.episode['title'], show.protocol.upper(),
-                          protocol_id, sxe, show.id, show.quality['quality']['name']))
 
         for series_title, episode_title, protocol, protocol_id, sxe, sonarr_id, quality in queue:
             hash_id = hashit(f'{self.server.id}{series_title}{sxe}')
@@ -143,7 +157,8 @@ class SonarrAPI(object):
                     }
                 }
             )
+            
         if influx_payload:
             self.dbmanager.write_points(influx_payload)
         else:
-            self.logger.debug("No data to send to influx for sonarr instance, discarding.")
+            self.logger.debug("No Sonarr queue data to send to InfluxDB for server %s.", self.server.id)
